50fc744d462a73b906dfd7a95ec44135e2ac9445
Toufiqur Rahman <toufiq.dev@gmail.com>
2024-11-13 15:05:10 +0600

    Simple http framework HighMark

--- web-server/highMark.js
+++ web-server/highMark.js
@@ -1,19 +1,50 @@
 const http = require("node:http");
+const fs = require("node:fs/promises");
 
 class HighMark {
   constructor() {
     this.server = http.createServer();
+    this.routes = {};
 
     this.server.on("request", (req, res) => {
-      res.end("Hello World");
+      // send a file back to the client
+      res.sendFile = async (path, mime) => {
+        const fileHandle = await fs.open(path, "r");
+        const fileStream = fileHandle.createReadStream();
+
+        res.setHeader("Content-Type", mime);
+
+        fileStream.pipe(res);
+      };
+      // set the status code of the response
+      res.status = (code) => {
+        res.statusCode = code;
+        return res;
+      };
+      res.json = (data) => {
+        res.setHeader("Content-Type", "application/json");
+        res.end(JSON.stringify(data));
+      };
+
+      // if routes object doesn't have a route for this request, send back 404
+      const routePath = req.method.toLowerCase() + req.url;
+      if (!this.routes[routePath]) {
+        return res.status(404).json({ error: "Route not found" });
+      }
+
+      this.routes[routePath](req, res);
     });
   }
 
-  listen = (port, cb) => {
+  route(method, path, cb) {
+    this.routes[method + path] = cb;
+  }
+
+  listen(port, cb) {
     this.server.listen(port, () => {
       cb();
     });
-  };
+  }
 }
 
 module.exports = HighMark;
--- web-server/server.js
+++ web-server/server.js
@@ -4,10 +4,16 @@ const PORT = 8080;
 
 const server = new HighMark();
 
-server.route("GET", "/upload", (req, res) => {
-  res.status(200).sendFile("./public/index.html", "text/html");
+server.route("get", "/", (req, res) => {
+  res.sendFile("./public/index.html", "text/html");
+});
+server.route("get", "/styles.css", (req, res) => {
+  res.sendFile("./public/styles.css", "text/css");
+});
+server.route("get", "/script.js", (req, res) => {
+  res.sendFile("./public/script.js", "text/javascript");
 });
 
 server.listen(PORT, () => {
-  console.log("Server listening ");
+  console.log("Server listening on port", PORT);
 });
