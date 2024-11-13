a1722bf239e8b46a769fac233beb02ba8ffaae07
Toufiqur Rahman <toufiq.dev@gmail.com>
2024-11-13 23:54:02 +0600

    Simple blog server with proxy

---
+++ simple-blog/public/db.js
@@ -0,0 +1,16 @@
+const USERS = [
+  { id: 1, name: "Liam Brown", username: "liam23", password: "string" },
+  { id: 2, name: "Meredith Green", username: "merit.sky", password: "string" },
+  { id: 3, name: "Ben Adams", username: "ben.poet", password: "string" },
+];
+
+const POSTS = [
+  {
+    id: 1,
+    title: "This is a post title",
+    body: "orem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged.",
+    userId: 1,
+  },
+];
+
+module.exports = { USERS, POSTS };
---
+++ simple-blog/server.js
@@ -0,0 +1,54 @@
+const highMark = require("../web-server/highMark");
+const { USERS, POSTS } = require("./public/db");
+
+const PORT = 8080;
+
+const server = new highMark();
+
+server.route("get", "/", (req, res) => {
+  res.sendFile("./public/index.html", "text/html");
+});
+server.route("get", "/login", (req, res) => {
+  res.sendFile("./public/index.html", "text/html");
+});
+server.route("get", "/styles.css", (req, res) => {
+  res.sendFile("./public/styles.css", "text/css");
+});
+server.route("get", "/scripts.js", (req, res) => {
+  res.sendFile("./public/scripts.js", "text/javascript");
+});
+
+server.route("get", "/api/posts", (req, res) => {
+  const posts = POSTS.map((post) => {
+    const user = USERS.find((user) => user.id === post.userId);
+
+    post.author = user.name;
+    return post;
+  });
+
+  res.status(200).json(posts);
+});
+
+server.route("post", "/api/login", (req, res) => {
+  let body = "";
+  req.on("data", (chunk) => {
+    body += chunk;
+  });
+
+  req.on("end", () => {
+    const { username, password } = JSON.parse(body);
+    const user = USERS.find((user) => user.username === username);
+
+    if (user && user.password === password) {
+      res.status(200).json({ message: "Login successful" });
+    } else {
+      res.status(401).json({ error: "Invalid credentials" });
+    }
+  });
+});
+
+server.listen(PORT, () => {
+  console.log("Server listening on port", PORT);
+});
+
+
